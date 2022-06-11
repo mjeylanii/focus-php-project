50efa73cc97d834b95589792e61f66f55044e0d4
Mohamed Jeylani <95252871+mJeylanii@users.noreply.github.com>
2022-06-11 17:00:21 +0300

    Update Database.php

--- core/Database.php
+++ core/Database.php
@@ -14,7 +14,6 @@ class Database
         $dsn = $config['dsn'] ?? '';
         $user = $config['user'] ?? '';
         $password = $config['password'] ?? '';
-
         $this->pdo = new \PDO($dsn, $user, $password);
         $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
     }
